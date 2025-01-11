1dfe51fac381fda3e17d7320b1f13fa18fdeafd9
Akagbor <akagborgilbert@4gmail.com>
2025-01-11 10:22:08 +0000

    Modify LibraryManagementSwing.java
    0323080100 Davor Bright modified the file

--- src/librarymanagementswing/LibraryManagementSwing.java
+++ src/librarymanagementswing/LibraryManagementSwing.java
@@ -12,6 +12,10 @@ import java.awt.event.ActionListener;
 import java.sql.*;
 
 public class LibraryManagementSwing {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/LibraryDB";
+    private static final String DB_USER = "root"; // Replace with your MySQL username
+    private static final String DB_PASSWORD = ""; // Replace with your MySQL password
+    private Connection connection;
 
     public static void main(String[] args) {
         
